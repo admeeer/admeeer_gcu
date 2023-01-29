af4f014ffda38f4cb58829f88e3df171b8cb5c82
alexpeltier <alexpeltier@localhost.localdomain>
2023-01-29 12:04:22 -0600

    change file structure, finish producer consumer

--- 315/ProducerConsumer/producer_consumer.cpp
+++ 315/ProducerConsumer/producer_consumer.cpp
@@ -12,7 +12,7 @@
 
 typedef int bfr;
 typedef int ctr;
-#define BUFFER_SIZE 10
+#define BUFFER_SIZE 3
 
 // mutex lock
 pthread_mutex_t mutex;
@@ -24,11 +24,16 @@ sem_t full, empty;
 // buffer
 bfr buffer[BUFFER_SIZE];
 
-pthread_t tid; // thread id
+// thread id
+pthread_t pid;
+pthread_t cid;
+
 pthread_attr_t attr; // attributes
 
+// counter for buffer
 ctr counter;
 
+// function declarations
 void* producer(void *arg);
 void* consumer(void *arg);
 
@@ -46,6 +51,7 @@ void init() {
     // create empty semaphore and initialize it to TRUE
     sem_init(&empty, 0, 1);
 
+    // pthread attributes
     pthread_attr_init(&attr);
 
     counter = 0;
@@ -56,60 +62,76 @@ void* producer(void *arg) {
 
 
     while(TRUE) {
-
+        // create some item, give it a value
         bfr item = 1;
-
+        // wait for empty sempahore to be true
         sem_wait(&empty);
-
+        // lock the mutex
         pthread_mutex_lock(&mutex);
-
+        // produce and throw into stack
         while(counter < BUFFER_SIZE) {
             sleep(1);
             std::cout << "Producer put item in stack at slot " << counter << std::endl;
             buffer[counter] = item;
             counter++;
         }
-
+        // were done producing, unlock mutex
         pthread_mutex_unlock(&mutex);
-
+        // flip full semaphore
         sem_post(&full);
 
 
     }
 
+    return NULL;
+
 }
 
 void* consumer(void *arg) {
-
+    // null item
     bfr item;
 
     while(TRUE){
-
+        // wait for full semaphore to be true
         sem_wait(&full);
-
+        // lock mutex
         pthread_mutex_lock(&mutex);
-
+        // consume stack
         while(counter > 0) {
             sleep(1);
             std::cout << "Consumer consumed item in stack at slot " << counter-1 << std::endl;
             buffer[(counter-1)] = item;
             counter--;
         }
+        // we've consumed all the items, unlock mutex
+        pthread_mutex_unlock(&mutex);   
+        // say that were empty
+        sem_post(&empty);
 
 
     }
 
+    return NULL;
+
 }
 
 int main() {
-std::cout << "Hello, World" << std::endl;
+
+// call init function
 init();
 
-pthread_create(&tid, &attr, producer, NULL);
-std::cout << "Created producer thread with ID " << tid << std::endl;
-pthread_create(&tid, &attr, consumer, NULL);
-std::cout << "Created consumer thread with ID " << tid << std::endl;
+//create pthreads
+pthread_create(&pid, &attr, producer, NULL);
+std::cout << "Created producer thread with ID " << pid << std::endl;
+pthread_create(&cid, &attr, consumer, NULL);
+std::cout << "Created consumer thread with ID " << cid << std::endl;
 
-
+/*
+we never catch the exit of the pthreads because we never close them
+so, this is kind of like a while(true)
+*/ 
+pthread_join(pid, NULL);
+pthread_join(cid, NULL);
+ 
 return 0;
 }
